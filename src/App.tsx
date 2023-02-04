39773c5a4a34995966643b06e337aaaaf2db689e
ninest <parth.kabra@gmail.com>
2023-02-03 19:03:30 -0500

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <main>
+      <h1 className="font-bold text-6xl">Robbinghood</h1>
+    </main>
+  );
+}
+
+export default App;

1e72ac0a275d492f445b82af4a52a34ca380202c
ninest <parth.kabra@gmail.com>
2023-02-15 18:00:58 -0500

    Add basic line chart

--- src/components/charts/line-chart.stories.tsx
+++ src/components/charts/line-chart.stories.tsx
@@ -1,5 +1,31 @@
-export const LineChartDemo = () => (
-  <>
-    <div className="text-xl">This is a line chart demo</div>
-  </>
-);
+import { LineChart } from "./line-chart";
+
+export const LineChartDemo = () => {
+  return (
+    <>
+      what:
+      <LineChart
+        data={[
+          { day: 0, value: 1 },
+          { day: 1, value: 2 },
+          { day: 2, value: 6 },
+          { day: 3, value: 12 },
+          { day: 4, value: 17 },
+          { day: 5, value: 13 },
+          { day: 6, value: 12 },
+          { day: 7, value: 19 },
+          { day: 8, value: 0 },
+          { day: 9, value: 1 },
+          { day: 10, value: 100 },
+          { day: 11, value: 105 },
+          { day: 12, value: 105 },
+          { day: 13, value: 105 },
+          { day: 14, value: 105 },
+          { day: 15, value: 215 },
+          { day: 16, value: 200 },
+          { day: 17, value: 3 },
+        ]}
+      />
+    </>
+  );
+};
--- src/components/charts/line-chart.tsx
+++ src/components/charts/line-chart.tsx
@@ -1,5 +1,64 @@
-interface LineChartProps {}
+// import { scaleLinear } from "d3-scale";
+// import { line } from "d3-shape";
+import { scaleLinear, line as d3Line, curveNatural, max } from "d3";
+import useMeasure from "react-use-measure";
+import { DataPoint } from "../../types";
 
-export const LineChart = ({}: LineChartProps) => {
-  return <></>;
+interface LineChartProps {
+  data: DataPoint[];
+}
+export const LineChart = ({ data }: LineChartProps) => {
+  const [ref, bounds] = useMeasure();
+
+  return (
+    <div ref={ref} className="h-56 w-full">
+      <LineChartInner data={data} width={bounds.width} height={bounds.height} />
+    </div>
+  );
+};
+
+interface LineChartInner {
+  width: number;
+  height: number;
+  data: DataPoint[];
+}
+const LineChartInner = ({ width, height, data }: LineChartInner) => {
+  const margin = { top: 10, right: 10, bottom: 20, left: 24 };
+
+  // X: day
+  const getX = (d: DataPoint) => d.day;
+  // Y: value
+  const getY = (d: DataPoint) => d.value;
+
+  const firstDay = data[0].day;
+  const lastDay = data.at(-1)!.day;
+
+  const xScale = scaleLinear()
+    .domain([firstDay, lastDay])
+    .range([margin.left, width - margin.right]);
+
+  const maxValue = Math.max(...data.map((d) => d.value));
+
+  const yScale = scaleLinear()
+    .domain([
+      0,
+      maxValue //+ 0.1 * maxValue, // curve may make line go out of bounds, so add
+    ])
+    .range([height - margin.bottom, margin.top]);
+
+  const dataAsList: [number, number][] = data.map((d) => [d.day, d.value]);
+
+  const line = d3Line()
+    // .curve(curveNatural)
+    .x((d) => xScale(d[0]))
+    .y((d) => yScale(d[1]));
+  const pathD = line(dataAsList)!;
+
+  return (
+    <>
+      <svg width={width} height={height} className="text-green-600">
+        <path d={pathD} fill="none" stroke="currentColor" strokeWidth={2} />
+      </svg>
+    </>
+  );
 };
